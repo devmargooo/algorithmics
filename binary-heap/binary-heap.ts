5cc43d31ed33cbdbdbc88e4e804a39c7a4c50563
devmargooo <devmargooo@gmail.com>
2024-11-04 09:49:05 +0300

    print heap

--- binary-heap/binary-heap.ts
+++ binary-heap/binary-heap.ts
@@ -5,17 +5,6 @@ class BinaryHeap {
         return Math.floor(Math.log2(index))
     }
 
-
-    public print() {
-        const height = this.getLevel(this.heap.length);
-
-        const index = 0;
-
-        for (let i = 0; i < height; i++) {
-
-        }
-    }
-
     private getNode(index: number) {
         return {
             index,
@@ -23,7 +12,7 @@ class BinaryHeap {
         }
     }
 
-    public getIndexesTree() {
+    public getPrintTree() {
         const height = this.getLevel(this.heap.length);
         const tree = [[this.getNode(0)]];
 
@@ -33,12 +22,12 @@ class BinaryHeap {
             for (let j = 0; j < prev_line.length; j++) {
                 const parent = prev_line[j];
                 if (!parent.value) {
-                    continue
+                    continue;
                 }
-                const left = parent.index * 2 + 1;
-                new_line.push(this.getNode(left));
-                const right = parent.index * 2 + 2;
-                new_line.push(this.getNode(right));
+                const left = this.getNode(parent.index * 2 + 1);
+                new_line.push(left);
+                const right = this.getNode(parent.index * 2 + 2);
+                new_line.push(right);
             }
             if (new_line.length)  {
                 tree.push(new_line);
@@ -47,8 +36,30 @@ class BinaryHeap {
 
         return tree;
     }
+
+    public print() {
+        const print_tree = this.getPrintTree();
+        const print_height = print_tree.length;
+        for (let i = 0; i < print_tree.length; i++) {
+            let print_string = this.getSpace(print_height - i);
+            const line = print_tree[i];
+            for (let j = 0; j < line.length; j++) {
+                print_string += line[j].value;
+                print_string += ' ';
+            }
+            console.log(print_string);
+        }
+    }
+
+    private getSpace(length: number): string {
+        if (!length) {
+            return ''
+        }
+
+        return new Array(length).fill(' ').join('');
+    }
 }
 
 const heap = new BinaryHeap();
-const tree = heap.getIndexesTree();
-console.log(tree);
+heap.print();
+// console.log(tree);
